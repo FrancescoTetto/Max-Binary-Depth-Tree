4a160e4ad7b9ad65c27bc0d95522ae0e44380053
Francesco Stan <94371232+FrancescoTetto@users.noreply.github.com>
2024-09-01 23:18:26 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution(object):
+    def maxDepth(self, root):
+        if root is None:
+            return 0
+        else:
+            left_depth = self.maxDepth(root.left)
+            right_depth = self.maxDepth(root.right)
+            return max(left_depth, right_depth) + 1
+
+tree = TreeNode(1)
+tree.left = TreeNode(2)
+tree.right = TreeNode(3)
+tree.left.left = TreeNode(4)
+tree.left.right = TreeNode(5)
+
+solution = Solution()
+print(solution.maxDepth(tree))
+        
+
+
+        
+       
